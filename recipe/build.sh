a6a5b895bd4cfaa0dc4df58ba74f2a4054e2c2c4
Maksim Rakitin <mrakitin@bnl.gov>
2020-04-02 14:55:53 -0400

    Set AUTODOCKTOOLS_UTIL env var

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,21 @@
 ./install.sh -d $PREFIX
 
 BINARY_HOME=$PREFIX/bin
-UTILITIES_HOME=$PREFIX/MGLToolsPckgs/AutoDockTools/Utilities24
+UTILITIES_RPATH=MGLToolsPckgs/AutoDockTools/Utilities24
+UTILITIES_HOME=$PREFIX/${UTILITIES_RPATH}
+
 ln -s $UTILITIES_HOME/prepare_ligand4.py $BINARY_HOME/prepare_ligand4.py
 ln -s $UTILITIES_HOME/prepare_receptor4.py $BINARY_HOME/prepare_receptor4.py
+
+ACTIVATE=$PREFIX/etc/conda/activate.d/autodocktools.sh
+DEACTIVATE=$PREFIX/etc/conda/deactivate.d/autodocktools.sh
+
+# set up
+echo "export AUTODOCKTOOLS_UTIL=\$CONDA_DEFAULT_ENV/${UTILITIES_RPATH}"> $ACTIVATE
+
+# tear down
+echo "unset AUTODOCKTOOLS_UTIL" > $DEACTIVATE
+
+# clean up after self
+unset ACTIVATE
+unset DEACTIVATE
